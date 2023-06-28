9bd381d8a2d67c79905dccdd2df3cee3dacf4eab
Junnel Padilla <junnelpadilla307@gmail.com>
2023-06-28 00:25:05 -0700

    added prompt

---
+++ app.js
@@ -0,0 +1,10 @@
+const SVG = require('svg.js');
+
+function makeLogo () {
+    const text = prompt('Choose three characters for text logo: ');
+
+    const colorText = prompt('Choose the text of hte color(can be hexadecimal number: ');
+
+    const shapeType = parseInt(prompt('Choose the shape of your logo: '));
+
+}
