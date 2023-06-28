c5ef1aa07c3b3a32ed221e4f69dc0117d7f8f0cd
Junnel Padilla <junnelpadilla307@gmail.com>
2023-06-28 00:50:28 -0700

    added prompts and rendering the logo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const SVG = require('svg.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 function makeLogo () {
     // Prompt for choosing 3 characters for logo
@@ -36,5 +38,24 @@ function makeLogo () {
     // Creating SVG Document
     const svg = SVG().size(300, 200);
 
+    switch(shape) {
+        case 'triangle':
+            svg.polygon('100,0 200,200 0,200').fill(shapeColor);
+            break;
+        case 'square':
+            svg.rect(150, 150).move(75, 75).fill(shapeColor);
+            break;
+        case 'circle':
+            svg.circle(100).move(100, 50).fill(shapeColor);
+            break;
+    }
+
+    svg.text(text).move(150 - (text.length * 10), 125).font({fill: colorText, family: 'monospace', size: 30 });
+
+    fs.writeFileSync('logo.svg', svg.svg());
+
+    console.log('Logo Generated!')
 
 }
+
+makeLogo();
